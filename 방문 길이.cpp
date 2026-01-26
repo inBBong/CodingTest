ad40c8213d2e8ea35ae71e9e8b20964a31b3ff78
BBongCoding <ihhan01@gmail.com>
2026-01-27 00:09:44 +0900

    배열(3)

---
+++ 방문 길이.cpp
@@ -0,0 +1,157 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+using namespace std;
+class Point
+{
+private:
+	int x;
+	int y;
+public:
+	void Up() { y++;}
+	void Down() { y--;}
+	void Right() { x++;}
+	void Left() { x--;}
+	int GetX() { return x; }
+	int GetY() { return y; }
+	Point():x(0),y(0){}
+	Point(int x,int y):x(x),y(y){}
+};
+int solution(string dirs)
+{
+	Point p;
+	vector<pair<Point, char>> pos;
+	//pos.push_back({ 0,0 });
+	int cnt(0);
+	
+	for (int i = 0; i < dirs.size(); ++i)
+	{		
+		char D('X');
+		switch (dirs[i])
+		{
+		case 'U':
+			if(p.GetY()<5)
+			{
+				p.Up();
+				cnt++;
+				D = 'U';
+			}
+			break;
+		case 'D':
+			if (p.GetY() > -5)
+			{
+				p.Down();
+				cnt++;
+				D = 'D';
+			}
+			break;
+		case 'R':
+			if (p.GetX() < 5)
+			{
+				p.Right();
+				cnt++;
+				D = 'R';
+			}
+			break;
+		case 'L':
+			if (p.GetX() > -5)
+			{
+				p.Left();
+				cnt++;
+				D = 'L';
+			}
+			break;
+		}
+		for (int j = 0; j < pos.size(); ++j)
+		{
+			if (p.GetX() == pos[j].first.GetX() && p.GetY() == pos[j].first.GetY()&&D==pos[j].second&&D!='X')
+			{
+				cnt--; break;
+			}
+		}
+		pos.push_back({ p, D });
+		
+	}
+	return cnt;
+}
+int main()
+{
+	int answer;
+	answer=solution("LULLLLLLU");
+	cout << answer;
+}
+//정답코드
+//#include <string>
+//
+//using namespace std;
+//
+//bool visited[11][11][4]; // ❶ 특정 좌표에서 특정 방향으로 이동한 적이 있는지 체크하는 배열
+//
+//// ❷ 상하좌우로 좌표를 이동할 때 필요한 좌표의 오프셋 배열 
+//int dx[] = { 0, 1, 0, -1 };
+//int dy[] = { -1, 0, 1, 0 };
+//
+//// ❸ 각 문자에 해당하는 오프셋 배열의 인덱스를 반환
+//int todir(char dir) {
+//	int ret;
+//	if (dir == 'U') ret = 0;
+//	else if (dir == 'R') ret = 1;
+//	else if (dir == 'D') ret = 2;
+//	else ret = 3;
+//	return ret;
+//}
+//
+//// ❹ 특정좌표가 주어진 좌표평면을 벗어나는지 확인
+//bool isNotValid(int x, int y)
+//{
+//	return x < 0 || y < 0 || x > 10 || y > 10;
+//}
+//// ❺ 현재와 반대 방향의 오프셋 배열 인덱스 반환
+//int opposite_direction(int dir)
+//{
+//	return (dir + 2) % 4;
+//}
+//
+//int solution(string dirs)
+//{
+//	int answer = 0;
+//	int x = 5, y = 5; // ❻ 시작 좌표를 설정
+//
+//	for (auto c : dirs) {
+//		// ❼ 반복문을 순회하며 nx, ny는  x, y가 dirs대로 움직였을 때 위치가 됨 
+//		int dir = todir(c);
+//		int nx = x + dx[dir];
+//		int ny = y + dy[dir];
+//		// ❽ 좌표평면을 벗어난 경우 더 이상 이동하지 않음
+//		if (isNotValid(nx, ny)) {
+//			continue;
+//		}
+//
+//		// ❾  다음 좌표가 아직 방문하지 않은 좌표인 경우
+//		if (visited[y][x][dir] == false) {
+//			// ❿ 방문이 중복 체크되지 않도록 해당 경로의 양방향을 방문 체크
+//			visited[y][x][dir] = true;
+//			visited[ny][nx][opposite_direction(dir)] = true;
+//			answer++;
+//		}
+//		// ⓫ 현재좌표를 이동한 좌표로 업데이트
+//		x = nx;
+//		y = ny;
+//	}
+//	return answer;
+//}
+//
+//
+//
+//
+////아래 코드는 테스트 코드 입니다.
+//#include <iostream>
+//
+//int main()
+//{
+//	cout << solution("ULURRDLLU") << endl; // 7
+//	cout << solution("LULLLLLLU") << endl; // 7 
+//
+//	return 0;
+//}
--- 실패율.cpp
+++ 실패율.cpp
@@ -1,26 +1,141 @@
-#include<iostream>
+﻿#include<iostream>
 #include<vector>
 #include<algorithm>
 
 using namespace std;
 
-// ���۰��� ������ �������� ���� ���̵� ������ ���� �������� ���ϴ� �ڵ带 �ۼ�����!
-// ������ ���� : (���������� ���� ������ ���� Ŭ�������� ���� �÷��̾��� ��) / (���������� ���� �� �÷��̾��� ��)
-// ��ü �������� ������ N, ������ �̿��ϴ� ����ڰ� ���� ���� �ִ� ���������� ��ȣ�� ��� �迭 stages�� �־��� �� ��������
-// ���� ������������ ������������ ���������� ��ȣ�� ��� �ִ� �迭�� ��ȯ�ϵ��� solution() �Լ��� �ۼ��ϼ���.
-// ��������
-// 1. �������� ���� N�� 1 �̻� 500 ������ �ڿ���.
-// 2. stages�� ���̴� 1 �̻� 200,000 ����
-// 3. stages���� 1 �̻� N + 1 ������ �ڿ����� ����
-//		-�� �ڿ����� ����ڰ� ���� ���� ���� �������� ��ȣ�� ��Ÿ��
-//		-�� N + 1�� ������ ��������, �� N���� Ŭ������ ����ڸ� ��Ÿ��
-// 4. ���� �������� ���� ���������� �ִٸ� ���� ��ȣ�� ���������� ���� ���� ��
-// 5. ���������� ������ ������ ���� ��� �ش� ���������� �������� 0���� ����
-vector<int> solution(int N, vector<int>stages)
-{
+// 슈퍼게임 개발자 오셀리의 게임 난이도 조절을 위한 실패율을 구하는 코드를 작성하자!
+// 실패율 정의 : (스테이지에 도달 했으나 아직 클리어하지 못한 플레이어의 수) / (스테이지에 도달 한 플레이어의 수)
+// 전체 스테이지 개수가 N, 게임을 이용하는 사용자가 현재 멈춰 있는 스테이지의 번호가 담긴 배열 stages가 주어질 때 실패율이
+// 높은 스테이지부터 내림차순으로 스테이지의 번호가 담겨 있는 배열을 반환하도록 solution() 함수를 작성하세요.
+// 제약조건
+// 1. 스테이지 개수 N은 1 이상 500 이하의 자연수.
+// 2. stages의 길이는 1 이상 200,000 이하
+// 3. stages에는 1 이상 N + 1 이하의 자연수가 존재
+//		-각 자연수는 사용자가 현재 도전 중인 스테이지 번호를 나타냄
+//		-단 N + 1은 마지막 스테이지, 즉 N까지 클리어한 사용자를 나타냄
+// 4. 만약 실패율이 같은 스테이지가 있다면 작은 번호의 스테이지가 먼저 오면 됨
+// 5. 스테이지에 도달한 유저가 없는 경우 해당 스테이지의 실패율은 0으로 간주
 
+//내 코드 (정답코드에서 pair, compare 함수의 아이디어만 얻음)
+bool compare(pair<int, double>& a, pair<int, double>& b)
+{
+	if (a.second == b.second)
+		return a.first < b.first;
+	return a.second > b.second;
 }
+vector<int> solution(int N, vector<int>& stages)
+{
+	vector<int> PersonPerStage;//(스테이지에 도달 한 플레이어의 수)
+	vector<int> NonClearPerStage;//(스테이지에 도달 했으나 아직 클리어하지 못한 플레이어의 수)
+	vector<pair<int,double>> failPercent(N);//(스테이지에 도달 했으나 아직 클리어하지 못한 플레이어의 수)
+
+	vector<int> answer;//정답
+	PersonPerStage.assign(N, 0);
+	NonClearPerStage.assign(N, 0);
+	//answer.assign(N, 0);
+	//failPercent.assign(N,0);	
+	for (int i = 0; i < stages.size(); ++i)
+	{
+		for (int j = 0; j < stages[i]; j++)
+		{
+			if (j == N)break;
+			PersonPerStage[j]++;
+			if (j == stages[i] - 1)
+				NonClearPerStage[j]++;
+		}
+	}
+	for(int i=0;i<N;++i)
+	{
+		failPercent[i].first = i+1;
+		failPercent[i].second = static_cast<double>(NonClearPerStage[i]) / static_cast<double>(PersonPerStage[i]);
+	}
+	sort(failPercent.begin(), failPercent.end(), compare);
+	for (int i = 0; i < N; ++i)
+	{
+		answer.push_back(failPercent[i].first);
+	}
+	//sort(answer.rbegin(), answer.rend(), failPercent);
+	return answer;
+}
+
 int main()
 {
+	vector<int> v = { 2,1,2,6,2,4,3,3 };
+	vector<int> a;
+	a=solution(5, v);
+
+	for (auto ca : a)
+		cout << ca << endl;
 
 }
+//정답코드
+//#include <vector>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//// ❶ 문제에서 요구하는 조건대로 실패율을 정렬하는 함수 
+//bool compare(pair<int, float>& a, pair<int, float>& b) {
+//	if (a.second == b.second)
+//		return a.first < b.first;
+//	return a.second > b.second;
+//}
+//
+//vector<int> solution(int N, vector<int> stages) {
+//
+//	vector<int> answer; // ❷ answer는 최종 답 
+//	vector<float> challenger(N + 2, 0.0);  // ❸ challenger는 각 스테이지에 도달한 적이 있는 도전자의 수  
+//	vector<float> fail(N + 2, 0.0); // ❹ fail는 특정 스테이지에 실패한 도전자의 수
+//
+//	// ❺ 각 스테이지의 인원을 기준으로 특정 스테이지에서 실패한 인원수와, 각 스테이지에 도전한 적이 있는 인원수를 구함 
+//	for (int i = 0; i < stages.size(); i++) {
+//		for (int j = 1; j <= stages[i]; j++)
+//			challenger[j]++;
+//
+//		fail[stages[i]]++;
+//	}
+//	// ❻ failRatio는 실패율을 저장, first는 stage정보이고 second는 실패율 
+//	vector<pair<int, float>> failRatio(N);
+//
+//	// ❼ 스테이지 정보 및 실패율을 저장  
+//	for (int i = 0; i < N; i++) {
+//		failRatio[i].first = i + 1;
+//
+//		if (challenger[i + 1] == 0)
+//			failRatio[i].second = 0;
+//		else
+//			failRatio[i].second = fail[i + 1] / challenger[i + 1];
+//	}
+//
+//	// ➑ 계산한 실패율을 문제에서 제시한 조건에 맞게 정렬
+//	sort(failRatio.begin(), failRatio.end(), compare);
+//
+//	// ❾ 최종 답을 반환하기 위해 실패율을 저장
+//	for (int i = 0; i < N; i++) {
+//		answer.push_back(failRatio[i].first);
+//	}
+//
+//	return answer;
+//}
+//
+//
+//
+////아래 코드는 테스트 코드 입니다.
+//
+//#include <iterator>
+//#include <iostream>
+//void print(vector<int> vec)
+//{
+//	copy(vec.begin(), vec.end(), std::ostream_iterator<int>(cout, " "));
+//	cout << endl;
+//}
+//
+//int main()
+//{
+//	print(solution(5, { 2, 1, 2, 6, 2, 4, 3, 3 })); // 3 4 2 1 5
+//	print(solution(4, { 4, 4, 4, 4, 4 }));          // 4 1 2 3 
+//
+//	return 0;
+//}
+//시간복잡도 : O(M*N+NlogN)
