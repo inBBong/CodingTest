c486272ce400846f4af89f07477e39c3d73275fa
BBongCoding <ihhan01@gmail.com>
2026-02-02 16:07:10 +0900

    해시(2)

--- 문자열 해싱을 이용한 검색 함수 만들기.cpp
+++ 문자열 해싱을 이용한 검색 함수 만들기.cpp
@@ -1,10 +1,40 @@
-#include<iostream>
+﻿#include<iostream>
 #include<vector>
 #include<string>
+#include<cstdlib>
+
 using namespace std;
+//각 쿼리 배열에 있는 문자열이 string_list의 문자열 배열에 있는지 여부를 확인해야 합니다.문자열이 있으면 true, 없으면 false가 됩니다. 
+//각 문자열에 대해서 문자열의 존재 여부를 배열 형태로 반환하는 solution() 함수를 작성해주세요.
+//hash(s) =(s[0] + s[1]*p + s[2]*p^2 + ...s[n-1]*p^n-1)mod m
+//내 코드
+int Uhash(string s, int mod)
+{
+	int result(0);
+	int p = 1;
+	for (int i = 0; i < s.size(); ++i)
+	{
+		int val = s[i] - 'a' + 1;
+		result += (val * p) % mod;
+		p *= 31;
+	}
+	return result;
+}
 vector<bool>solution(vector<string> string_list, vector<string> query_list)
 {
-
+	vector<int> key;
+	vector<bool>answer;
+	for (auto cs : string_list)
+		key.push_back(Uhash(cs, string_list.size()));
+	
+	for (auto cq : query_list)
+	{
+		if (key.end() != find(key.begin(), key.end(), Uhash(cq, string_list.size())))
+			answer.push_back(true);
+		else
+			answer.push_back(false);
+	}
+	return answer;
 }
 int main()
 {
@@ -16,3 +46,72 @@ int main()
 		cout << ca<<" ";
 
 }
+////정답코드
+//#include <vector>
+//#include <string>
+//#include <unordered_set>
+//
+//using namespace std;
+//
+//// ❶ 다항 해시 함수 구현
+//long long polynomial_hash(const string& str) {
+//    const int p = 31;  // 소수
+//    const long long m = 1000000007;  // 버킷 크기
+//    long long hash_value = 0;
+//
+//    for (char c : str) {
+//        hash_value = (hash_value * p + c) % m;
+//    }
+//
+//    return hash_value;
+//}
+//
+//vector<bool> solution(vector<string> string_list, vector<string> query_list) {
+//    unordered_set<long long> hash_set;
+//
+//    //❷ string_list의 각 문자열에 대해 다항 해시값을 계산하고 저장
+//    for (const string& str : string_list) {
+//        long long hash = polynomial_hash(str);
+//        hash_set.insert(hash);
+//    }
+//
+//    vector<bool> result;
+//
+//    //❸  query_list의 각 문자열이 string_list에 있는지 확인하고 result에 추가
+//    for (const string& query : query_list) {
+//        long long query_hash = polynomial_hash(query);
+//        bool found = (hash_set.find(query_hash) != hash_set.end());
+//        result.push_back(found);
+//    }
+//    // ❹ query_list의 문자열이 hash에 있는지 결과가 저장된 result를 반환
+//    return result;
+//}
+//
+//
+////아래 코드는 테스트 코드 입니다.
+//#include <iterator>
+//#include <iostream>
+//void print(vector<bool> vec)
+//{
+//    copy(vec.begin(), vec.end(), std::ostream_iterator<bool>(cout, " "));
+//    cout << endl;
+//}
+//
+//int main()
+//{
+//    //true를 출력하면 1이되고 false를 출력하면 0
+//    print(solution({ "apple", "banana", "cherry" }, { "banana", "kiwi", "melon", "apple" })); // 1 0 0 1
+//    return 0;
+//
+//}
+// 내 코드와 다른 점:
+// 나는 키 값을 담은 int 형vector를 사용했고, 정답코드에서는 long long 형unordered_set을 사용함.
+// long long 자료형이 표현할 수 있는 정수의 범위가 훨씬 많음 900해임 ㅅㅂㅋㅋ int는 21억정도
+// mod 값을 나는 string_list의 사이즈로 했는데 정답코드에서는 100000007을 사용함.
+// 이건 내가 개념이 덜 잡혀서 그런듯.string_list의 사이즈로 하면 경우의 수가 string_list의 사이즈밖에 안나와서
+// 해시 충돌이 많이 일어날 것 같음.
+// 또 해시 함수를 나는 N-1번째 문자에 p^(N-1)로 계산했는데 정답코드는 0번째 문자에 p^(N-1)로 계산함.
+// 가중치는 정방향, 역방향 어떻게 해도 상관이 없다고 함.
+// 
+// 시간복잡도 :O(N + K)
+//
---
+++ 완주하지 못한 선수.cpp
@@ -0,0 +1,131 @@
+﻿#include<iostream>
+#include<vector>
+#include<string>
+#include<unordered_set>
+using namespace std;
+// 단 한명의 선수만 완주실패. 참가자 배열 participant와 완주자 배열 complettion 배열이 주어졌을때 완주하지 못한
+// 선수의 이름을 반환하는 solution 함수를 만드세요.
+long long Uhash(string s)
+{
+	int m = 100000007;
+	long long hash_value(0);
+	int p = 31;
+	for (char c:s)
+	{		
+		hash_value = (hash_value*p + c) % m;
+	}
+	return hash_value;
+}
+string solution(vector<string> participant, vector<string>completion)
+{
+	unordered_set<long long>hashvalue;
+	for (auto cc : completion)
+		hashvalue.insert(Uhash(cc));
+	for (auto cp : participant)
+	{
+		if (hashvalue.find(Uhash(cp)) != hashvalue.end())
+			continue;
+		else
+			return cp;
+	}
+	return "0";
+}
+int main()
+{
+	vector<string> p = { "leo", "kiki", "eden" };
+	vector<string> c = { "eden","kiki" };
+	vector<string> p2 = { "mislav", "stanko", "mislav","ana"};
+	vector<string> c2 = { "stanko", "ana","mislav" };
+	cout << solution(p2, c2);
+}
+//정답코드
+//#include <string>
+//#include <vector>
+//#include <unordered_map>
+//
+//using namespace std;
+//
+//string solution(vector<string> participant, vector<string> completion) {
+//	//❶ participant를 나타낼 해시테이블 
+//	unordered_map<string, int> ph;
+//
+//	//❷ 각 참가자들을 해시 테이블에 추가( 키 : 이름, 값 : 해당 이름의 명 수)
+//	for (int i = 0; i < participant.size(); i++)
+//		ph[participant[i]]++;
+//
+//
+//	//❸ 참가자 정보가 저장된 해시 테이블에서, 완주한 선수들을 제외
+//	for (int i = 0; i < completion.size(); i++)
+//	{
+//		ph[completion[i]]--;
+//		//❹ 해시테이블에서 특정 이름의 인원이 0명이면, 해시 테이블에서 삭제
+//		if (ph[completion[i]] == 0)
+//			ph.erase(ph.find(completion[i]));
+//	}
+//	//❺ 마지막 남은 한 선수의 이름을 반환 
+//	return ph.begin()->first;
+//}
+//
+//
+//
+////아래 코드는 테스트 코드 입니다.
+//
+//#include <iostream>
+//
+//int main()
+//{
+//	cout << solution({ "leo", "kiki", "eden" }, { "eden", "kiki" }) << endl; // "leo"
+//	cout << solution({ "marina", "josipa", "nikola", "vinko", "filipa" }, { "josipa", "filipa", "marina", "nikola" }) << endl; // "vinko"
+//	cout << solution({ "mislav", "stanko", "mislav", "ana" }, { "stanko", "ana", "mislav" }) << endl; // "mislav"
+//	return 0;
+//
+//}
+// 내 코드와 다른점:
+// 내 코드에서는 문자열 해싱을 사용해서 참가자 해쉬로 완주자 배열을 순회하면서
+// 이름을 못찾으면 해당 이름을 반환하는 식으로 구현하였지만 이 코드의 한계는 중복되는 사람의
+// 이름은 찾아내지 못한다는 것이었다.
+// 정답코드에서는 이미 해시의 기능을 하는 unordered_map을 사용하고 있다.
+// unordered_map<string, int> ph; 형식을 사용하여, 참가자 이름과, 그 이름의 사람 수를
+// 저장한다.
+// 완주자 배열을 순회하면서 탐색한 이름을 키값으로 하는 ph의 int 값을 -1 한다.
+// int 값이 0이되면 맵에서 erase 한다.
+// 순회를 마치면 ph에서 값이 1인 string 하나만 남을 것이다.
+// string 을 반환하면 끝!
+// 확실히 정답코드가 간결하군.. 아직 낯선 자료구조들도 많이 써봐야할것같다.
+// 시간복잡도 :O(N)
+// 또다른 정답코드:
+//#include <string>
+//#include <vector>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//string solution(vector<string> participant, vector<string> completion) {
+//
+//	sort(participant.begin(), participant.end());
+//	sort(completion.begin(), completion.end());
+//
+//	for (int i = 0; i < completion.size(); i++)
+//	{
+//		if (participant[i] != completion[i])
+//			return participant[i];
+//	}
+//
+//	return participant[participant.size() - 1];
+//}
+//
+//
+//
+////아래 코드는 테스트 코드 입니다.
+//
+//#include <iostream>
+//
+//int main()
+//{
+//	cout << solution({ "leo", "kiki", "eden" }, { "eden", "kiki" }) << endl; // "leo"
+//	cout << solution({ "marina", "josipa", "nikola", "vinko", "filipa" }, { "josipa", "filipa", "marina", "nikola" }) << endl; // "vinko"
+//	cout << solution({ "mislav", "stanko", "mislav", "ana" }, { "stanko", "ana", "mislav" }) << endl; // "mislav"
+//	return 0;
+//
+//}
+//시간복잡도 :O(NlogN)
